ddc7663081cd8ec7814c0cac9099a7353ad02b52
Francis Hwang <sera@fhwang.net>
2014-01-23 13:00:26 -0500

    Test differences symmetrically.

--- test/json_deep_compare_test.rb
+++ test/json_deep_compare_test.rb
@@ -3,6 +3,34 @@ $: << '.'
 require 'lib/json-deep-compare'
 
 class DocumentComparisonTestCase < Test::Unit::TestCase
+  def assert_symmetrically_different(
+    lval, rval, message_template = nil, sub_pairs = nil
+  )
+    sub_pairs = [sub_pairs] if sub_pairs and sub_pairs.first.is_a?(String)
+    comparison1 = JsonDeepCompare::DocumentComparison.new(lval, rval)
+    assert !comparison1.equal?
+    if message_template
+      message1 = message_template
+      sub_pairs.each_with_index do |sub_pair, i|
+        message1 = message1.
+          sub(/:left#{i}/, sub_pair.first).
+          sub(/:right#{i}/, sub_pair.last)
+      end
+      assert_equal(message1, comparison1.difference_message)
+    end
+    comparison2 = JsonDeepCompare::DocumentComparison.new(rval, lval)
+    assert !comparison2.equal?
+    if message_template
+      message2 = message_template
+      sub_pairs.each_with_index do |sub_pair, i|
+        message2 = message2.
+          sub(/:left#{i}/, sub_pair.last).
+          sub(/:right#{i}/, sub_pair.first)
+      end
+      assert_equal(message2, comparison2.difference_message)
+    end
+  end
+
   def test_detects_difference_in_atoms
     left_value = {
       'total_rows' => 2,
@@ -30,33 +58,26 @@ class DocumentComparisonTestCase < Test::Unit::TestCase
         }
       ]
     }
-    comparison = JsonDeepCompare::DocumentComparison.new(
-      left_value, right_value
-    )
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .rows :nth-child(1) > .doc > .sub_document > .one\" expected to be \"two\" but was \"1\"", 
-      comparison.difference_message
+    assert_symmetrically_different(
+      left_value, right_value, 
+      "\":root > .rows :nth-child(1) > .doc > .sub_document > .one\" expected to be :left0 but was :right0",
+      ["two".inspect, "1".inspect]
     )
   end
 
   def test_detects_differences_in_keys
     left_value = {"doc" => {"foo" => "bar"}}
     right_value = {"doc" => {"biz" => "bang", "bing" => nil}}
-    comparison = JsonDeepCompare::DocumentComparison.new(
-      left_value, right_value
-    )
-    assert !comparison.equal?
+    assert_symmetrically_different(left_value, right_value)
   end
 
   def test_detects_difference_in_type
     lval = {"foo" => {'bar' => 'bang'}}
     rval = {"foo" => [3,4,5]}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .foo\" expected to be {\"bar\"=>\"bang\"} but was [3, 4, 5]",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .foo\" expected to be :left0 but was :right0",
+      [{"bar" => "bang"}.inspect, [3,4,5].inspect]
     )
   end
 
@@ -67,55 +88,56 @@ class DocumentComparisonTestCase < Test::Unit::TestCase
     end
     lval = {"foo" => lval_hash}
     rval = {"foo" => [3,4,5]}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .foo\" expected to be Hash {\"A\"=>\"A\", \"B\"=>\"B\", \"C\"=>\"C\", \"D\"=>\"D... but was [3, 4, 5]",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .foo\" expected to be :left0 but was :right0",
+      [
+        "Hash {\"A\"=>\"A\", \"B\"=>\"B\", \"C\"=>\"C\", \"D\"=>\"D...", 
+        [3,4,5].inspect
+      ]
     )
   end
 
   def test_lists_multliple_differences
     lval = {'one' => {'two' => 'three', 'four' => 'five'}, 'six' => 'seven'}
     rval = {'one' => {'two' => 'TWO', 'four' => 'five'}, 'six' => 'SIX'}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .one > .two\" expected to be \"three\" but was \"TWO\"\n\":root > .six\" expected to be \"seven\" but was \"SIX\"",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .one > .two\" expected to be :left0 but was :right0\n\":root > .six\" expected to be :left1 but was :right1",
+      [["three".inspect, "TWO".inspect], ["seven".inspect, "SIX".inspect]]
     )
   end
 
   def test_detects_difference_between_array_and_nil
     lval = {'one' => [1,2,3]}
     rval = {'one' => nil}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .one\" expected to be [1, 2, 3] but was nil",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .one\" expected to be :left0 but was :right0",
+      [[1,2,3].inspect, 'nil']
     )
   end
 
   def test_shows_first_string_difference
     lval = {'body' => "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal."}
     rval = {'body' => "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .body\" differs starting at char 103: \"..., conceived in Liberty, and ded...\" differs from \"..., conceived in liberty, and ded...\"",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .body\" differs starting at char 103: :left0 differs from :right0",
+      [
+        "..., conceived in Liberty, and ded...".inspect,
+        "..., conceived in liberty, and ded...".inspect
+      ]
     )
   end
 
   def test_shows_string_difference_start
     lval = {'rev' => "22-23c92a95665bb692313229c8224b7088"}
     rval = {'rev' => "23-54a5106f8c522a57d6d4c6963bc36611"}
-    comparison = JsonDeepCompare::DocumentComparison.new(lval, rval)
-    assert !comparison.equal?
-    assert_equal(
-      "\":root > .rev\" differs starting at char 1: \"22-23c92a95665bb6...\" differs from \"23-54a5106f8c522a...\"",
-      comparison.difference_message
+    assert_symmetrically_different(
+      lval, rval,
+      "\":root > .rev\" differs starting at char 1: :left0 differs from :right0",
+      ["22-23c92a95665bb6...".inspect, "23-54a5106f8c522a...".inspect]
     )
   end
 
